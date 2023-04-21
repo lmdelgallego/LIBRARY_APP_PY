20297c4fe52949f03e01ce8f5819a5df48fd6a89
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2023-04-21 18:38:30 -0500

    ✨feature: 🚀 Add SQLLite

--- main.py
+++ main.py
@@ -1,6 +1,40 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+# import sqlite3
+
+
+# db = sqlite3.connect("new-new-books-collection.db")
+# cursor = db.cursor()
+#
+# cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) "
+#                "NOT NULL, rating FLOAT NOT NULL)")
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///new-new-books-collection.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy()
+
+db.init_app(app)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+    #Optional: this will allow each book object to be identified by its title when printed.
+    def __repr__(self):
+        return f'<Book {self.title}>'
+
+with app.app_context():
+    db.create_all()
+    # # #CREATE RECORD
+    # new_book = Book(id=1, title="Harry Potter", author="J. K. Rowling", rating=9.3)
+    # print(new_book)
+    # db.session.add(new_book)
+    # db.session.commit()
 
 all_books = [
     # {
@@ -18,7 +52,6 @@ def home():
 
 @app.route("/add", methods=['GET', 'POST'])
 def add():
-
     if request.method == 'POST':
         new_book = {
             "title": request.form.get('title'),
@@ -35,4 +68,3 @@ def add():
 
 if __name__ == "__main__":
     app.run(debug=True)
-
