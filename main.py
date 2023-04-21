35d1e2b768f99a81dfbe3a2ce3e6dd5912c7c7ae
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2023-04-21 18:00:26 -0500

    ✨feature: 🚀 Initial Commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+all_books = [
+    {
+        "title": "Harry Potter",
+        "author": "J. K. Rowling",
+        "rating": 9,
+    }
+]
+
+
+@app.route('/')
+def home():
+    return render_template("index.html", books=all_books)
+
+
+@app.route("/add")
+def add():
+    return render_template("add.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
