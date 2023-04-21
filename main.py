b24d7621082fe75414419c8a1f93ace02718c7bb
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2023-04-21 18:05:30 -0500

    ✨feature: 🚀 Implement list validation and handler add button

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 all_books = [
-    {
-        "title": "Harry Potter",
-        "author": "J. K. Rowling",
-        "rating": 9,
-    }
+    # {
+    #     "title": "Harry Potter",
+    #     "author": "J. K. Rowling",
+    #     "rating": 9,
+    # }
 ]
 
 
